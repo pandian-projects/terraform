e8b9ffd1aa4dbb276d87e6191fd1723a2f01f025
stonecoldd <j.pandian@gmail.com>
2026-04-28 21:59:29 +0530

    Initial Terraform project with modules and remote backend

---
+++ env/dev/variables.tf
@@ -0,0 +1,9 @@
+variable "vpc_cidr" {}
+variable "subnet_cidr" {}
+variable "vpc_name" {}
+variable "managed_by" {}
+variable "ami" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "my_ip" {}
+variable "region" {}
