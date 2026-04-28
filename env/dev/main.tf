335bd1ea2c4c6222776ac3cafa88fe0fe55841dd
stonecoldd <j.pandian@gmail.com>
2026-04-28 21:59:29 +0530

    Initial Terraform project with modules and remote backend

---
+++ env/dev/main.tf
@@ -0,0 +1,21 @@
+module "vpc" {
+  source = "E:/Terraform/module/vpc"
+
+  vpc_cidr    = var.vpc_cidr
+  subnet_cidr = var.subnet_cidr
+  vpc_name    = var.vpc_name
+  managed_by  = var.managed_by
+  my_ip       = var.my_ip
+}
+
+module "ec2" {
+  source = "E:/Terraform/module/ec2"
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = module.vpc.subnet_id
+  sg_id         = module.vpc.sg_id
+  key_name      = var.key_name
+  vpc_name      = var.vpc_name
+  managed_by    = var.managed_by
+}
